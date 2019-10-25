a8ee3db4be788cef6c8dae901225c41b6b883f71
K C Gaurab <s1900524@edu.bc.fi>
2019-10-25 14:22:48 +0300

    added login and fixed create blog feature

--- src/App.js
+++ src/App.js
@@ -1,23 +1,188 @@
-import React from 'react';
+import React from 'react'
+import Blog from './components/Blog'
+import Notification from './components/Notification'
+import blogService from './services/blogs'
+import loginService from './services/login'
 
-function App() {
+import {useState, useEffect} from 'react'
+
+const App = () => {
+  const [blogs, setBlogs] = useState([])
+  const [newBlog, setNewBlog] = useState({
+    title: '',
+    author: '',
+    url: '',
+    likes: '',
+  })
+  const [errormessage, setErrorMessage] = useState(null)
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    blogService.getAll().then(initialBlogs => {
+      setBlogs(initialBlogs)
+    })
+  }, [])
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
+  const handleLogin = async event => {
+    event.preventDefault()
+    console.log('logging in with', username, password)
+
+    try {
+      const user = await loginService.login({
+        username,
+        password,
+      })
+      window.localStorage.setItem('loggedBlogUser', JSON.stringify(user))
+
+      blogService.setToken(user.token)
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      setErrorMessage('Wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  }
+
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+        <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({target}) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+        <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({target}) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>
+  )
+
+  const logOut = event => {
+    event.preventDefault()
+    setUser(null)
+    setUsername('')
+    setPassword('')
+    // window.localStorage.removeItem('loggedBlogUser')
+    window.localStorage.clear()
+  }
+
+  const handleTitleChange = event => {
+    event.preventDefault()
+    setNewBlog({...newBlog, title: event.target.value})
+  }
+
+  const handleAuthorChange = event => {
+    event.preventDefault()
+    setNewBlog({...newBlog, author: event.target.value})
+  }
+
+  const handleUrlChange = event => {
+    event.preventDefault()
+    setNewBlog({...newBlog, url: event.target.value})
+  }
+
+  const handleLikesChange = event => {
+    event.preventDefault()
+    setNewBlog({...newBlog, likes: event.target.value})
+  }
+
+  const addBlog = event => {
+    event.preventDefault()
+    const blogObj = {
+      title: newBlog.title,
+      author: newBlog.author,
+      url: newBlog.url,
+      likes: newBlog.likes,
+      id: blogs.length + 1,
+    }
+
+    blogService.create(blogObj).then(data => {
+      setBlogs(blogs.concat(data))
+      setNewBlog('')
+    })
+  }
+
+  const blogForm = () => (
+    <form onSubmit={addBlog}>
+      <div>
+        <label>
+          Title:
+          <input type="text" onChange={handleTitleChange} />
+        </label>
+      </div>
+
+      <div>
+        <label>
+          Author:
+          <input type="text" onChange={handleAuthorChange} />
+        </label>
+      </div>
+
+      <div>
+        <label>
+          Url:
+          <input type="url" onChange={handleUrlChange} />
+        </label>
+      </div>
+
+      <div>
+        <label>
+          Likes
+          <input type="number" onChange={handleLikesChange} />
+        </label>
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  )
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Blogs</h1>
+
+      <Notification message={errormessage} />
+
+      <h2>Login</h2>
+
+      {user === null ? (
+        loginForm()
+      ) : (
+        <div>
+          <p>{user.name} is logged in</p><button onClick={logOut}>
+            log out
+          </button>
+          {blogForm()}
+
+          {blogs.map(blog => (
+            <Blog key={blog.id} blog={blog} />
+          ))}
+        </div>
+      )}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/services/blogs.js
+++ src/services/blogs.js
@@ -1,9 +1,38 @@
 import axios from 'axios'
 const baseUrl = '/api/blogs'
 
+
+
+let token = null
+
+//get the token and adds 'bearer' string
+const setToken = newToken => {
+  token = `bearer ${newToken}`
+}
+
 const getAll = () => {
   const request = axios.get(baseUrl)
   return request.then(response => response.data)
 }
 
-export default { getAll }
+// create will take on newObject and add header with authorization property
+// that will have token as its key
+const create = newObj => {
+  const config = {
+    headers: { Authorization: token },
+  }
+  // new object will be posted along with config(with the token that was extracted)
+  const request =  axios.post(baseUrl, newObj, config)
+  console.log(newObj)
+  return request.then(response => response.data)
+  
+}
+
+//update takes in id and newObject to update the blog 
+const update = async (id, newBlog) => {
+  const response =  await axios.put(`${ baseUrl } /${id}`, newBlog)
+  return response.data
+
+}
+
+export default { getAll, create, update, setToken }
